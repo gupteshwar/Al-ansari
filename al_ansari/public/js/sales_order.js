c4e4bbc940fbee0ed7aa5f923188c3d3aeef3903
shreyasn.ext <shreyasn.ext@elastic.run>
2023-02-17 15:22:15 +0530

    phase_2_change

--- al_ansari/public/js/sales_order.js
+++ al_ansari/public/js/sales_order.js
@@ -3,11 +3,6 @@ frappe.ui.form.on("Sales Order",{
         item_rate(frm)
         validate_posting_date(frm)
     },
-    on_submit: function(frm){
-        if(frm.doc.docstatus ==1){
-            blanket_order_details(frm)
-        }
-    }
 })
 
 
@@ -19,21 +14,6 @@ function item_rate(frm){
                 frappe.throw(__("Item rate is below price list rate"))
             }
         })
-}
-
-function blanket_order_details(frm){
-    if (frm.doc.items["0"].blanket_order){
-      
-        var blanket_order = frm.doc.items["0"].blanket_order
-        var bo_doc = frappe.model.get_doc("Blanket Order", blanket_order)
-        console.log(bo_doc)
-        $.each(bo_doc.items, function(index, row){  
-            var d = bo_doc.add_child("sales_details");
-                d.name = frm.doc.name
-                d.grand_total = frm.doc.grand_total
-        })
-    }
-
 }
 
 function validate_posting_date(frm) {
