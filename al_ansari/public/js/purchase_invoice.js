7a2a2f0465e2ae863fb9cb6e4ab1fe4dfe69026b
User <nehatalokar@gmail.com>
2023-04-06 15:09:57 +0530

    feat:Phase2-Payment Request Amount Should Not Exceed PO Amount, PDC workflow and validation on dates, remove validation on PI, field addition on item master

--- al_ansari/public/js/purchase_invoice.js
+++ al_ansari/public/js/purchase_invoice.js
@@ -1,20 +1,9 @@
 frappe.ui.form.on('Purchase Invoice',{
     before_save:function (frm) {
-        // validate_posting_and_supp_inv_date(frm);
         // validate_posting_date(frm)
     }
 
 })
-
-function validate_posting_and_supp_inv_date(frm) {
-   
-    if (frm.posting_date && frm.doc.bill_date){
-        if (frm.doc.bill_date != frm.posting_date){
-                frappe.throw(__("Supplier Invoice Date should be same as Posting Date"))
-        }
-    }
-    
-}
 
 function validate_posting_date(frm) {
     if(frm.posting_date){
