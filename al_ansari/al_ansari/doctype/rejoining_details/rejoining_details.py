59489b705083cebcc5ac4c5b9351d2f0023ee00e
User <nehatalokar@gmail.com>
2023-05-12 17:33:27 +0530

    Allow Rejoining Details cancellation

--- al_ansari/al_ansari/doctype/rejoining_details/rejoining_details.py
+++ al_ansari/al_ansari/doctype/rejoining_details/rejoining_details.py
@@ -3,9 +3,9 @@
 
 import frappe
 from frappe.model.document import Document
+import json
 
 class RejoiningDetails(Document):
-	# pass
 
 	# update working status on employee master on submission
 	def validate(self):
@@ -13,8 +13,6 @@ class RejoiningDetails(Document):
 		if existing_doc and self.is_new() and not self.amended_from:
 			frappe.throw("A record is already created against the leave application") 
 		self.days_difference = frappe.utils.date_diff(self.actual_rejoining_date,self.initial_rejoin_date)
-		# if self.days_difference == 0:
-		# 	frappe.throw("Record cannot be saved as the Intial Rejoining date is same as the Actual Rejoining date")
 
 	def on_submit(self):
 		if self.status == "Rejoined":
@@ -27,11 +25,11 @@ class RejoiningDetails(Document):
 				leave_doc.leave_approver = frappe.get_value("Employee",self.employee,['leave_approver'])
 				leave_doc.to_date = frappe.utils.add_days(self.initial_rejoin_date,self.days_difference-1)
 				leave_doc.follow_via_email = 1
-				leave_doc.rejoining_doc = self.name
+				# leave_doc.rejoining_doc = self.name
+				leave_doc.rejoining_details_ref = self.name
 				leave_doc.status = 'Approved'
 				leave_doc.save()
-				# leave_doc.submit()
-				# frappe.msgprint("LWP marked. Please check and submit the same")
+				leave_doc.submit()
 
 			# mark working status on emp master
 			if self.docstatus == 1:
@@ -39,8 +37,36 @@ class RejoiningDetails(Document):
 				emp_rec.working_status = "Working"
 				emp_rec.save()	
 				frappe.msgprint("Employee working status updated in master")
-			# else:
-			# 	frappe.throw("Can't update as the rejoin date has to match the current date")
 
 			# update the rejoining entry on respective leave application
 			frappe.db.set_value("Leave Application",self.leave_application,"rejoining_details_ref",self.name)
+
+	def on_cancel(self):
+		if self.lwp_application:
+			lwp_application = frappe.get_doc("Leave Application",self.lwp_application)
+			lwp_application.cancel()
+
+		if self.leave_application:
+			frappe.db.set_value('Leave Application',self.leave_application,'rejoining_details_ref','')
+
+		emp_rec = frappe.get_doc("Employee",{'name':self.employee})
+		emp_rec.working_status = "On Leave"
+		emp_rec.save()	
+		frappe.msgprint("Employee working status reverted in master")
+
+@frappe.whitelist()
+def validate_rejoining_record(leave):
+	leave = frappe.get_doc(json.loads(leave))
+	rejoing_rec = frappe.db.get_values("Rejoining Details", {"leave_application": leave.name}, "name", as_dict=True)
+	if rejoing_rec :
+		rejoing_rec = frappe.get_doc("Rejoining Details", rejoing_rec[0]['name'])
+	else :
+		rejoing_rec = frappe.new_doc("Rejoining Details")
+		rejoing_rec.employee = leave.employee
+		rejoing_rec.initial_rejoin_date = leave.rejoin_date
+		rejoing_rec.leave_type = leave.leave_type
+		rejoing_rec.status = "On Leave"
+		rejoing_rec.leave_application = leave.name
+		rejoing_rec.actual_rejoining_date = frappe.utils.nowdate()
+		rejoing_rec.days_difference = frappe.utils.date_diff(frappe.utils.nowdate(),leave.rejoin_date)
+	return rejoing_rec.as_dict()
