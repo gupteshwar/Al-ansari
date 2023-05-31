1b0afce89bbf551e01cd4fa7d95ef654df041807
User <nehatalokar@gmail.com>
2023-05-31 22:40:50 +0530

    Overtime Calculator make payroll costcenter filter non-mandatory

--- al_ansari/al_ansari/doctype/overtime_calculator/overtime_calculator.py
+++ al_ansari/al_ansari/doctype/overtime_calculator/overtime_calculator.py
@@ -23,6 +23,181 @@ class OvertimeCalculator(Document):
 			frappe.throw("The Overtime Calculator Details table cannot be empty.")
 		additional_salary_entry(self)	
 
+	@frappe.whitelist()
+	def get_employees_on_oc(self):
+		filters = self.make_filters()
+		cond =get_filter_condition(filters)
+		emp_list = frappe.db.sql(
+			"""
+				select
+					t1.name,t1.employee_name,t1.hourly_rate,t1.grade
+				from
+					`tabEmployee` t1
+				where
+					t1.status = 'Active'
+					%(cond)s 
+			"""
+				%{
+					'cond':cond
+				},
+				as_dict=True
+			)
+		print("emp_list === ",emp_list)
+		if not emp_list:
+			error_msg = _(
+				"No employees found for the mentioned criteria:"
+			)
+			if self.branch:
+				error_msg += "<br>" + _("Branch: {0}").format(frappe.bold(self.branch))
+			if self.reporting_manager:
+				error_msg += "<br>" + _("Reporting Manager: {0}").format(frappe.bold(self.reporting_manager))
+			if self.payroll_cost_center:
+				error_msg += "<br>" + _("Payroll Cost Center: {0}").format(frappe.bold(self.payroll_cost_center))
+			frappe.throw(error_msg, title=_("No employees found"))
+
+		# return emp_list
+		return self.get_calculation_for_shift(emp_list)
+
+	@frappe.whitelist()
+	def get_calculation_for_shift(self,emp_list):
+		
+		if emp_list:
+			for emp in emp_list:
+				productive_hours_ratio = frappe.get_value("Employee Grade",emp["grade"],"productive_hours_ratio")
+				h_overtime = frappe.db.sql(""" 
+					Select 
+						ec.name,
+						ec.employee,
+						ec.employee_name,
+						max(ec.actual_hours) as actual_hours,
+						ec.overtime_rate,
+						ec.productive_hours,
+						st.shift_hours ,
+						ec.time,
+						ec.is_holiday
+					from `tabEmployee Checkin` ec, `tabShift Type` st,`tabEmployee` e  
+					where 
+						ec.employee = e.name and 
+						e.default_shift = st.name and 
+						DATE(ec.time) >= %s and 
+						DATE(ec.time) <= %s and 
+						ec.log_type = 'OUT' and
+						e.working_status != 'On Leave' and
+						e.name = %s and
+						ec.is_holiday = 1
+						group by date(ec.time)
+						order by date(ec.time) desc;
+					""",(self.from_date,self.to_date,emp["name"]),as_dict=1)
+
+				nh_overtime = frappe.db.sql(""" 
+					Select 
+						ec.name,
+						ec.employee,
+						ec.employee_name,
+						max(ec.actual_hours) as actual_hours,
+						ec.overtime_rate,
+						ec.productive_hours,
+						st.shift_hours ,
+						ec.time,
+						ec.is_holiday
+					from `tabEmployee Checkin` ec, `tabShift Type` st,`tabEmployee` e  
+					where 
+						ec.employee = e.name and 
+						e.default_shift = st.name and 
+						DATE(ec.time) >= %s and 
+						DATE(ec.time) <= %s and 
+						ec.log_type = 'OUT' and
+						e.working_status != 'On Leave' and
+						e.name = %s and
+						ec.is_holiday = 0
+						group by date(ec.time)
+						order by date(ec.time) desc;
+					""",(self.from_date,self.to_date,emp["name"]),as_dict=1)
+				emp.update({
+					"productive_hours_ratio":productive_hours_ratio
+					})
+				final_shift_total = 0.00
+				h_actual_total = 0.00
+				holiday_overtime_total = 0.00
+				h_shift_total = 0.00
+				nh_actual_total = 0.00
+				non_holiday_overtime_total = 0.00
+				nh_shift_total = 0.00
+				if h_overtime :
+					emp.update({
+						"employee_name":h_overtime[0]["employee_name"] ,
+						})
+				if nh_overtime:
+					emp.update({
+						"employee_name":nh_overtime[0]["employee_name"],
+						})
+
+				if h_overtime:
+					ot_amt = 0.00
+
+					for h_ot in h_overtime:
+						h_actual_total += h_ot["actual_hours"]
+						holiday_overtime_total += h_ot["actual_hours"] 
+					
+					emp.update({
+						"holiday_overtime_rate":h_overtime[0]["overtime_rate"],
+						"holiday_overtime":holiday_overtime_total,
+						"holiday_actual_hours":h_actual_total,
+						"h_shift_total":h_shift_total
+						})
+				else:
+					emp.update({
+						"holiday_overtime_rate":0 ,
+						"holiday_overtime":holiday_overtime_total,
+						"holiday_actual_hours":h_actual_total,
+						"h_shift_total":h_shift_total
+						})
+
+				if nh_overtime:	
+					ot_amt = 0.00
+
+					for nh_ot in nh_overtime:
+						nh_actual_total += nh_ot["actual_hours"]
+						non_holiday_overtime_total += nh_ot["actual_hours"]-nh_ot["shift_hours"] if (nh_ot["actual_hours"]>nh_ot["shift_hours"]) else 0
+						nh_shift_total += nh_ot["shift_hours"]
+					
+					emp.update({
+						"non_holiday_overtime_rate":nh_overtime[0]["overtime_rate"],
+						"non_holiday_overtime":non_holiday_overtime_total,
+						"non_holiday_actual_hours":nh_actual_total,
+						"nh_shift_total":nh_shift_total
+						})
+
+				else:
+					emp.update({
+						"non_holiday_overtime_rate":0 ,
+						"non_holiday_overtime":non_holiday_overtime_total,
+						"non_holiday_actual_hours":nh_actual_total,
+						"nh_shift_total":nh_shift_total
+						})
+
+				emp.update({
+					"shift_hours":round(emp["h_shift_total"] + emp["nh_shift_total"],2),
+					"hourly_rate":emp["hourly_rate"]
+					})
+
+		return emp_list	
+
+	def make_filters(self):
+		filters = frappe._dict()
+		filters["reports_to"] = self.reporting_manager
+		filters["branch"] = self.branch
+		filters["payroll_cost_center"] = self.payroll_cost_center 
+
+		return filters
+
+def get_filter_condition(filters):
+	cond = ""
+	for f in ["branch","reports_to","payroll_cost_center"]: 
+		if filters.get(f):
+			cond += " and t1." + f + " = " + frappe.db.escape(filters.get(f))
+	return cond
+
 @frappe.whitelist()
 def autofill_employees(payroll_entry):
 	payroll_entry = frappe.get_doc(json.loads(payroll_entry))
@@ -39,7 +214,6 @@ def autofill_employees(payroll_entry):
 		employee_name,hourly_rate,grade = frappe.get_value('Employee',emp.employee,["employee_name","hourly_rate","grade"])
 		emp_list.append({"name":emp.employee,"employee_name":employee_name ,"hourly_rate": hourly_rate,"productive_hours_ratio":frappe.get_value("Employee Grade",grade,["productive_hours_ratio"])})
 		
-	print("EMPLIST==",emp_list)
 	if emp_list:
 		valid_emp_list = validate_employees_on_oc(payroll_entry.start_date,payroll_entry.end_date,emp_list)
 	if valid_emp_list:
@@ -177,167 +351,6 @@ def validate_employees_on_oc(from_date,to_date,emp_list):
 			else:
 				emp.update({
 					"non_holiday_overtime_rate": 0 ,#frappe.get_value("Employee",emp["name"],["nh_ot_rate"]),
-					"non_holiday_overtime":non_holiday_overtime_total,
-					"non_holiday_actual_hours":nh_actual_total,
-					"nh_shift_total":nh_shift_total
-					})
-
-			emp.update({
-				"shift_hours":round(emp["h_shift_total"] + emp["nh_shift_total"],2),
-				"hourly_rate":emp["hourly_rate"]
-				})
-
-	return emp_list
-
-@frappe.whitelist()
-def get_employees_on_oc(from_date,to_date,branch,reporting_manager,payroll_cost_center):
-	emp_list = []
-
-	if reporting_manager == " " and branch !=" ":
-		emp_list = frappe.db.get_list('Employee',
-		    filters={
-		        'branch': branch,
-		        'status': 'Active',
-		        'payroll_cost_center':payroll_cost_center
-
-		    },
-		    fields=['name','employee_name','hourly_rate','grade']
-		)
-
-	if branch == " " and reporting_manager !=" ":
-		emp_list = frappe.db.get_list('Employee',
-		    filters={
-		        'reports_to': reporting_manager,
-		        'status': 'Active',
-		        'payroll_cost_center':payroll_cost_center
-		    },
-		    fields=['name','employee_name','hourly_rate','grade']
-		)
-
-	if branch != " " and reporting_manager != " ":
-		emp_list = frappe.db.get_list('Employee',
-		    filters={
-		    	'branch': branch,
-		        'reports_to': reporting_manager,
-		        'status': 'Active',
-		        'payroll_cost_center':payroll_cost_center
-		    },
-		    fields=['name','employee_name','hourly_rate','grade']
-		)
-
-	if branch == " " and reporting_manager ==" ":
-		frappe.throw("Please select either branch or reporting manager")
-
-	if emp_list:
-		for emp in emp_list:
-			productive_hours_ratio = frappe.get_value("Employee Grade",emp["grade"],"productive_hours_ratio")
-			h_overtime = frappe.db.sql(""" 
-				Select 
-					ec.name,
-					ec.employee,
-					ec.employee_name,
-					max(ec.actual_hours) as actual_hours,
-					ec.overtime_rate,
-					ec.productive_hours,
-					st.shift_hours ,
-					ec.time,
-					ec.is_holiday
-				from `tabEmployee Checkin` ec, `tabShift Type` st,`tabEmployee` e  
-				where 
-					ec.employee = e.name and 
-					e.default_shift = st.name and 
-					DATE(ec.time) >= %s and 
-					DATE(ec.time) <= %s and 
-					ec.log_type = 'OUT' and
-					e.working_status != 'On Leave' and
-					e.name = %s and
-					ec.is_holiday = 1
-					group by date(ec.time)
-					order by date(ec.time) desc;
-				""",(from_date,to_date,emp["name"]),as_dict=1)
-
-			nh_overtime = frappe.db.sql(""" 
-				Select 
-					ec.name,
-					ec.employee,
-					ec.employee_name,
-					max(ec.actual_hours) as actual_hours,
-					ec.overtime_rate,
-					ec.productive_hours,
-					st.shift_hours ,
-					ec.time,
-					ec.is_holiday
-				from `tabEmployee Checkin` ec, `tabShift Type` st,`tabEmployee` e  
-				where 
-					ec.employee = e.name and 
-					e.default_shift = st.name and 
-					DATE(ec.time) >= %s and 
-					DATE(ec.time) <= %s and 
-					ec.log_type = 'OUT' and
-					e.working_status != 'On Leave' and
-					e.name = %s and
-					ec.is_holiday = 0
-					group by date(ec.time)
-					order by date(ec.time) desc;
-				""",(from_date,to_date,emp["name"]),as_dict=1)
-			emp.update({
-				"productive_hours_ratio":productive_hours_ratio
-				})
-			final_shift_total = 0.00
-			h_actual_total = 0.00
-			holiday_overtime_total = 0.00
-			h_shift_total = 0.00
-			nh_actual_total = 0.00
-			non_holiday_overtime_total = 0.00
-			nh_shift_total = 0.00
-			if h_overtime :
-				emp.update({
-					"employee_name":h_overtime[0]["employee_name"] ,
-					})
-			if nh_overtime:
-				emp.update({
-					"employee_name":nh_overtime[0]["employee_name"],
-					})
-
-			if h_overtime:
-				ot_amt = 0.00
-
-				for h_ot in h_overtime:
-					h_actual_total += h_ot["actual_hours"]
-					holiday_overtime_total += h_ot["actual_hours"] 
-				
-				emp.update({
-					"holiday_overtime_rate":h_overtime[0]["overtime_rate"],
-					"holiday_overtime":holiday_overtime_total,
-					"holiday_actual_hours":h_actual_total,
-					"h_shift_total":h_shift_total
-					})
-			else:
-				emp.update({
-					"holiday_overtime_rate":0 ,
-					"holiday_overtime":holiday_overtime_total,
-					"holiday_actual_hours":h_actual_total,
-					"h_shift_total":h_shift_total
-					})
-
-			if nh_overtime:	
-				ot_amt = 0.00
-
-				for nh_ot in nh_overtime:
-					nh_actual_total += nh_ot["actual_hours"]
-					non_holiday_overtime_total += nh_ot["actual_hours"]-nh_ot["shift_hours"] if (nh_ot["actual_hours"]>nh_ot["shift_hours"]) else 0
-					nh_shift_total += nh_ot["shift_hours"]
-				
-				emp.update({
-					"non_holiday_overtime_rate":nh_overtime[0]["overtime_rate"],
-					"non_holiday_overtime":non_holiday_overtime_total,
-					"non_holiday_actual_hours":nh_actual_total,
-					"nh_shift_total":nh_shift_total
-					})
-
-			else:
-				emp.update({
-					"non_holiday_overtime_rate":0 ,
 					"non_holiday_overtime":non_holiday_overtime_total,
 					"non_holiday_actual_hours":nh_actual_total,
 					"nh_shift_total":nh_shift_total
