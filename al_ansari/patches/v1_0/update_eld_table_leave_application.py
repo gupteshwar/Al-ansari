ce1455f2ed39807512daf80ac3201a185da2bcf5
User <nehatalokar@gmail.com>
2023-07-07 18:09:54 +0530

    Patch v1_0 updated

--- al_ansari/patches/v1_0/update_eld_table_leave_application.py
+++ al_ansari/patches/v1_0/update_eld_table_leave_application.py
@@ -5,12 +5,10 @@ from al_ansari.al_ansari.customization.leave_application import split_entries_mo
 def execute():
 	leave_applications = frappe.get_all("Leave Application",
 			filters = [['docstatus','=',1],['leave_type','IN',('Sick Leave')]],
-			fields=["name","leave_type"])
-	print("Patch=",leave_applications)
+			fields=["name","leave_type"],limit=2)
 	for l_app in leave_applications:
 		leave_app = frappe.get_doc("Leave Application",l_app)
 		linked_ppl = frappe.db.get_value("Leave Type",l_app.leave_type,"partial_paid_leave")
-
 		from_date = leave_app.from_date.strftime('%Y-%m-%d')
 		to_date = leave_app.to_date.strftime('%Y-%m-%d')
 		if linked_ppl:
@@ -18,13 +16,16 @@ def execute():
 				monthly_breakup = split_entries_monthly(l_app.leave_type,from_date,to_date)
 				if monthly_breakup:
 					for mb in monthly_breakup:
-						if monthly_breakup[0][0] != from_date:
+						if type(monthly_breakup[0][0]) == type("Test"):
+							comp_date = from_date
+						else:
+							comp_date =leave_app.from_date
+						if monthly_breakup[0][0] != comp_date:
 							leave_app.append('eld_fraction_monthly',{
 									'from_date': from_date,
 									'to_date':frappe.utils.add_days(mb[0],days=-1),
 								})
 							leave_app.save()
-							print(leave_app.eld_fraction_monthly)	
 							add_fractional_eld(leave_app,linked_ppl)
 							leave_app.save()
 						
@@ -33,7 +34,6 @@ def execute():
 								'to_date':mb[1],
 							})
 						leave_app.save()
-						print(leave_app.eld_fraction_monthly)	
 						add_fractional_eld(leave_app,linked_ppl)
 						leave_app.save()
 					frappe.db.commit()
