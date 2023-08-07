e793c6966ffc6ff27a37d454f29a465ae1591c0a
User <nehatalokar@gmail.com>
2023-08-07 11:04:14 +0530

    chore:filter cost center in child table on customer for credit limit

--- al_ansari/public/js/customer.js
+++ al_ansari/public/js/customer.js
@@ -2,7 +2,9 @@ frappe.ui.form.on("Customer",{
     refresh:function(frm){
         cur_frm.set_query("cost_center","credit_limits",function(){
                 return {
-                    filters: [["company","=",frm.doc.credit_limits[0].company]]
+                    filters: [["company","=",frm.doc.credit_limits[0].company],
+                            ["is_group","!=","1"]
+                        ]
                 } 
             
         })
