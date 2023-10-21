dd6256aa338c6828c1c3946a21fe63c70a0498a9
halimas786 <halima.s@indictranstech.com>
2023-10-21 21:47:06 +0530

    cost center carry forward to items cost center

--- al_ansari/al_ansari/customization/purchase_invoice.py
+++ al_ansari/al_ansari/customization/purchase_invoice.py
@@ -5,3 +5,13 @@ def validate_cost_center(doc, method):
         for i in doc.items:
             if not i.purchase_order and not i.purchase_receipt:
                 i.cost_center = doc.cost_center
+            elif i.purchase_order:
+                purchase_order = frappe.get_doc('Purchase Order', i.purchase_order)
+                for so in purchase_order.items:
+                    if i.item_code == so.item_code:
+                        i.cost_center = so.cost_center
+            elif i.purchase_receipt:
+                purchase_receipt = frappe.get_doc('Purchase Receipt', i.purchase_receipt)
+                for so in purchase_receipt.items:
+                    if i.item_code == so.item_code:
+                        i.cost_center = so.cost_center
