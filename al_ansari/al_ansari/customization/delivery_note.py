aa2aa131ac49ba9e3d1e34801b6726805b523f3f
halimas786 <halima.s@indictranstech.com>
2023-10-23 19:01:17 +0530

    fix: carry forward cost center

--- al_ansari/al_ansari/customization/delivery_note.py
+++ al_ansari/al_ansari/customization/delivery_note.py
@@ -8,10 +8,10 @@ def validate_cost_center(doc, method):
             elif i.against_sales_order:
                 sales_order = frappe.get_doc('Sales Order', i.against_sales_order)
                 for so in sales_order.items:
-                    if i.item_code == so.item_code:
+                    if i.item_code == so.item_code and i.idx and so.idx:
                         i.cost_center = so.cost_center
             elif i.against_sales_invoice:
                 sales_invoice = frappe.get_doc('Sales Invoice', i.against_sales_invoice)
                 for so in sales_invoice.items:
-                    if i.item_code == so.item_code:
+                    if i.item_code == so.item_code and i.idx and so.idx:
                         i.cost_center = so.cost_center
