dcfc5cb4f1f7009b90b202d223183027413c01a3
User <nehatalokar@gmail.com>
2023-11-28 11:15:00 +0530

    chore:Don't allow rate change if line item has reference

--- al_ansari/public/js/purchase_receipt.js
+++ al_ansari/public/js/purchase_receipt.js
@@ -18,6 +18,11 @@ frappe.ui.form.on('Purchase Receipt',{
                     }
             })
         }
+        // var df = frappe.meta.get_docfield("Purchase Order Item","rate", cur_frm.doc.name);
+        // df.read_only = 1;
+        var event =""
+
+        hide_child_table_buttons(frm)
     },
 
     onload: function(frm) {
@@ -45,6 +50,70 @@ frappe.ui.form.on('Purchase Receipt',{
                 }
             });
         }
-    }
+        var event =""
 
+        hide_child_table_buttons(frm)
+    },
+
+    refresh: function(frm) {
+        var event =""
+        hide_child_table_buttons(event,frm)
+        frm.get_field('items').grid.cannot_add_rows = true;
+    }
 })
+
+frappe.ui.form.on('Purchase Receipt Item', {
+    form_render(frm, cdt, cdn){
+        var row = locals[cdt][cdn]
+        if (row.purchase_order || row.purchase_invoice){
+            frm.fields_dict.items.grid.wrapper.find('.grid-delete-row').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-duplicate-row').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-move-row').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-append-row').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-insert-row-below').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-insert-row').hide();
+            // frm.fields_dict.items.grid.wrapper.find('.clear-fix').prop('disabled','true')
+        }    
+    },
+
+    rate(frm,cdt,cdn) {
+        var row = locals[cdt][cdn]
+        if (row.purchase_order || row.purchase_invoice){
+            var event = 'rate'
+            hide_child_table_buttons(event,frm)
+            frm.fields_dict.items.grid.wrapper.find('.grid-delete-row').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-duplicate-row').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-move-row').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-append-row').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-insert-row-below').hide();
+            frm.fields_dict.items.grid.wrapper.find('.grid-insert-row').hide();
+        }  
+    }
+});
+
+function hide_child_table_buttons(event,frm) {
+    if(frm.doc.items) {
+
+        if(frm.doc.items[0].purchase_order || frm.doc.items[0].purchase_invoice){
+            if (event != 'rate'){
+                frm.fields_dict.items.grid.update_docfield_property("rate", "read_only", 1);
+
+            }
+
+            $('*[data-fieldname="items"]').find('.grid-remove-rows').hide();
+            console.log("1")
+            $('*[data-fieldname="items"]').find('.grid-add-multiple-rows').hide();
+            console.log("2")
+            $('*[data-fieldname="items"]').find('.grid-add-row').hide();
+            console.log("3")
+            $('*[data-fieldname="items"]').find('.grid-download').hide();
+            console.log("4")
+            $('*[data-fieldname="items"]').find('.grid-upload').hide();
+            console.log("5")
+            
+            console.log("6")
+        }
+        // frm.get_field('items').grid.cannot_add_rows = true;
+        // frm.get_field('items').grid.cannot_add_rows = true;
+    }
+}
