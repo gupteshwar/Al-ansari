bbff4351d68ed1a5409770ebc159e5c6d46f9608
User <nehatalokar@gmail.com>
2023-12-08 11:23:23 +0530

    chore:Asset Repair-remove commented code

--- al_ansari/al_ansari/customization/asset_repair.py
+++ al_ansari/al_ansari/customization/asset_repair.py
@@ -1,14 +1,6 @@
 import frappe
 
 def validate_asset_repair(doc, method):
-    # if doc.asset:
-    #     assetDoc = frappe.get_doc("Asset", doc.asset)
-    #     pi_total_amount = 0
-    #     if assetDoc.purchase_invoice:
-    #         pi_total_amount = frappe.get_value("Purchase Invoice", assetDoc.purchase_invoice, "grand_total")
-
-    #     if doc.repair_cost > pi_total_amount and pi_total_amount != 0:
-    #         frappe.throw("Asset Repair should Not Exceed the repair cost")
     if doc.repair_cost and doc.purchase_invoice:
         purchase_invoice_total = frappe.get_value("Purchase Invoice",doc.purchase_invoice,fieldname=['grand_total'])
         if doc.repair_cost < purchase_invoice_total:
