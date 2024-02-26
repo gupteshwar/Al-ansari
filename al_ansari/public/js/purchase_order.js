ee5326f86828dbfc1af67ba5f1aedc807110bf41
Midocean <manjeetkhatri008@gmail.com>
2024-02-26 12:44:40 +0530

    changes

--- al_ansari/public/js/purchase_order.js
+++ al_ansari/public/js/purchase_order.js
@@ -3,13 +3,13 @@ frappe.ui.form.on("Purchase Order",{
         // validate_posting_date(frm)
         item_rate(frm)
     },
-    cost_center: function(frm) {
-        frm.doc.items.forEach(function(item){
-            item.cost_center = frm.doc.cost_center
-            item.branches = frm.doc.branch
-        });
-        frm.refresh_field('items')
-    },
+    // cost_center: function(frm) {
+    //     frm.doc.items.forEach(function(item){
+    //         item.cost_center = frm.doc.cost_center
+    //         item.branches = frm.doc.branch
+    //     });
+    //     frm.refresh_field('items')
+    // },
     branch: function(frm) {
         frm.doc.items.forEach(function(item){
             item.cost_center = frm.doc.cost_center
