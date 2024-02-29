24269d48f1065d7b4990ec38bc8c6b856f2ab88e
Midocean <manjeetkhatri008@gmail.com>
2024-02-29 12:22:26 +0530

    po and dn fixes

--- al_ansari/al_ansari/customization/purchase_order.py
+++ al_ansari/al_ansari/customization/purchase_order.py
@@ -6,7 +6,8 @@ def validate_cost_center(doc, method):
             if not i.sales_order and not i.cost_center:
                 i.cost_center = doc.cost_center
             else:
-                sales_order = frappe.get_doc('Sales Order', i.sales_order)
-                for so in sales_order.items:
-                    if i.item_code == so.item_code and i.idx == so.idx:
-                        i.cost_center = so.cost_center
+                if i.sales_order:
+                    sales_order = frappe.get_doc('Sales Order', i.sales_order)
+                    for so in sales_order.items:
+                        if i.item_code == so.item_code and i.idx == so.idx:
+                            i.cost_center = so.cost_center
