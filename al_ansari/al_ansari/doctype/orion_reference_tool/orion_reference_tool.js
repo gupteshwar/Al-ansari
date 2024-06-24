087fee876bbb387c6852d674480f1ebd4b9966a1
ManjitKhatri <manjeetkhatri008@gmail.com>
2024-06-24 08:15:22 +0530

    post go feedback points

---
+++ al_ansari/al_ansari/doctype/orion_reference_tool/orion_reference_tool.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Indictrans and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Orion Reference Tool', {
+	// refresh: function(frm) {
+
+	// }
+});
